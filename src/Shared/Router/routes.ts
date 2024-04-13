a94d0773bfcc11b86e98b1a3a994683e22abaad7
ReneNieto <rene.nieto@idoagency.com>
2024-04-13 15:39:08 -0600

    fixed tailwind base, added register and login pages and routes

--- src/Shared/Router/routes.ts
+++ src/Shared/Router/routes.ts
@@ -1,20 +1,28 @@
 const routes = {
-  Home: {
-    path: "/",
-    name: "Home",
-  },
-  Test: {
-    path: "/test",
-    name: "Test",
-  },
-  Agencies: {
-    path: "/agencies",
-    name: "Agencies",
-  },
-  Profile: {
-    path: "/profile",
-    name: "profile",
-  },
-};
+	Home: {
+		path: "/",
+		name: "Home",
+	},
+	Test: {
+		path: "/test",
+		name: "Test",
+	},
+	Agencies: {
+		path: "/agencies",
+		name: "Agencies",
+	},
+	Profile: {
+		path: "/profile",
+		name: "profile",
+	},
+	Login: {
+		path: "/login",
+		name: "Login",
+	},
+	Register: {
+		path: "/Register",
+		name: "Register",
+	},
+}
 
-export default routes;
+export default routes
---
+++ src/features/Login/Containers/Login.tsx
@@ -0,0 +1,7 @@
+export default function Login() {
+	return (
+		<div className="w-full h-screen bg-black">
+			<input type="password" name="" id="" />
+		</div>
+	)
+}
