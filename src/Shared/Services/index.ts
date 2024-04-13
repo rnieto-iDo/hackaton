329d93e4e828a6dad44cb68f565b6135e649f237
carlosguther <carlos.duarte@idoagency.com>
2024-04-12 22:05:06 -0600

    created axios instance

---
+++ src/Shared/Services/index.ts
@@ -0,0 +1,24 @@
+import axios, { AxiosInstance } from "axios";
+
+export enum PATH_LIST {
+  AGENCIES = "/agencies",
+}
+
+export const createAxiosInstance = (
+  // userData: IUserData,
+  PATH?: string
+): AxiosInstance => {
+  const baseURL = `${import.meta.env.VITE_API_BASE_URL as string}${PATH}`;
+
+  const headers = {
+    // Authorization: `Bearer ${userData.jwtToken}`,
+    "Content-Type": "application/json",
+  };
+
+  const clientAxios = axios.create({
+    baseURL,
+    headers,
+  });
+
+  return clientAxios;
+};
