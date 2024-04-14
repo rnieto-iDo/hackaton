86e9a6143f78e6076b52b43ceb3f81d924da2f93
ReneNieto <rene.nieto@idoagency.com>
2024-04-14 04:15:07 -0600

    added login and register, added theme colors to tailwind and ant

--- src/Shared/Router/index.tsx
+++ src/Shared/Router/index.tsx
@@ -3,24 +3,27 @@ import routes from "./routes"
 import Home from "../Components/Home"
 import { Agencies } from "../../features/agencies/Containers/Agencies"
 import Profile from "../../features/profile/Containers/Profile"
-import Login from "../../features/Login/Containers/Login"
-import Register from "../../features/Register/Containers/Register"
+import Login from "../../features/Signup/Containers/Signup"
 import { Destinations } from "../../features/destinations/Containers/Destinations"
+import Signup from "../../features/Signup/Containers/Signup"
 
 export default function Router() {
-    return (
-        <Routes>
-            <Route path={routes.Home.path} element={<Home />} />
-            <Route path={routes.Agencies.path} element={<Agencies />} />
-            <Route path={routes.Profile.path} element={<Profile />} />
-            <Route path={routes.Login.path} element={<Login />} />
-            <Route path={routes.Register.path} element={<Register />} />
-            <Route path={routes.DestinationsByAgency.path} element={<Destinations />} />
+	return (
+		<Routes>
+			<Route path={routes.Home.path} element={<Home />} />
+			<Route path={routes.Agencies.path} element={<Agencies />} />
+			<Route path={routes.Profile.path} element={<Profile />} />
+			<Route path={routes.Login.path} element={<Login />} />
+			<Route path={routes.Register.path} element={<Signup />} />
+			<Route
+				path={routes.DestinationsByAgency.path}
+				element={<Destinations />}
+			/>
 
-            <Route path="/404" element={<>404 page</>} />
+			<Route path="/404" element={<>404 page</>} />
 
-            {/* This route matches any page that doesn't exists, so it must be at the end of the Routes */}
-            <Route path="*" element={<Navigate to="/404" />} />
-        </Routes>
-    )
+			{/* This route matches any page that doesn't exists, so it must be at the end of the Routes */}
+			<Route path="*" element={<Navigate to="/404" />} />
+		</Routes>
+	)
 }
