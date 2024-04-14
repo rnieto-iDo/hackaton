93daad3d5dfbf52f9386d9ab847423d5a838bcf8
ReneNieto <rene.nieto@idoagency.com>
2024-04-13 18:38:17 -0600

    added login page and store

--- src/Shared/App/store.ts
+++ src/Shared/App/store.ts
@@ -1,14 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit"
 
-import { AgenciesReducer } from "../../features/agencies/Slices/agenciesSlice";
+import { AgenciesReducer } from "../../features/agencies/Slices/agenciesSlice"
+import UserSlice from "../../features/Login/Slices/UserSlice"
 
 export const store = configureStore({
-  reducer: {
-    // Add reducers here
-    agencies: AgenciesReducer,
-  },
-});
+	reducer: {
+		// Add reducers here
+		agencies: AgenciesReducer,
+		user: UserSlice,
+	},
+})
 
 // Infer the RootState and AppDispatch types from the store itself
-export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
---
+++ src/features/Login/Slices/UserSlice.ts
@@ -0,0 +1,24 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { IUserLogin } from "../Services/login"
+
+interface UserState {
+	user: IUserLogin | null
+}
+
+const initialState: UserState = {
+	user: null,
+}
+
+const userSlice = createSlice({
+	name: "user",
+	initialState,
+	reducers: {
+		setUser: (state, action: PayloadAction<IUserLogin>) => {
+			state.user = action.payload
+		},
+	},
+})
+
+export const { setUser } = userSlice.actions
+
+export default userSlice.reducer
