35f2e669bf2835779b54b610ecb38fabcb3fefa5
Alexander Sánchez <alexander.sanchez@idoagency.com>
2024-04-19 20:44:08 -0600

    create profile component

--- src/Shared/Router/index.tsx
+++ src/Shared/Router/index.tsx
@@ -1,27 +1,28 @@
-import { Routes, Route, Navigate } from "react-router-dom"
-import routes from "./routes"
-import Home from "../Components/Home"
-import { Agencies } from "../../features/agencies/Containers/Agencies"
-import Profile from "../../features/profile/Containers/Profile"
-import { Destinations } from "../../features/destinations/Containers/Destinations"
-import Signup from "../../features/Signup/Containers/Signup"
+import { Routes, Route, Navigate } from "react-router-dom";
+import routes from "./routes";
+import Home from "../Components/Home";
+import { Agencies } from "../../features/agencies/Containers/Agencies";
+import Profile from "../../features/profile/Containers/Profile";
+import { Destinations } from "../../features/destinations/Containers/Destinations";
+import Signup from "../../features/Signup/Containers/Signup";
 
 export default function Router() {
-	return (
-		<Routes>
-			<Route path={routes.Home.path} element={<Home />} />
-			<Route path={routes.Agencies.path} element={<Agencies />} />
-			<Route path={routes.Profile.path} element={<Profile />} />
-			<Route path={routes.Login.path} element={<Signup />} />
-			<Route
-				path={routes.DestinationsByAgency.path}
-				element={<Destinations />}
-			/>
+  return (
+    <Routes>
+      <Route path={routes.Home.path} element={<Home />} />
+      <Route path={routes.Agencies.path} element={<Agencies />} />
+      <Route path={routes.Profile.path} element={<Profile />} />
+      <Route path={routes.Login.path} element={<Signup />} />
 
-			<Route path="/404" element={<>404 page</>} />
+      <Route
+        path={routes.DestinationsByAgency.path}
+        element={<Destinations />}
+      />
 
-			{/* This route matches any page that doesn't exists, so it must be at the end of the Routes */}
-			<Route path="*" element={<Navigate to="/404" />} />
-		</Routes>
-	)
+      <Route path="/404" element={<>404 page</>} />
+
+      {/* This route matches any page that doesn't exists, so it must be at the end of the Routes */}
+      <Route path="*" element={<Navigate to="/404" />} />
+    </Routes>
+  );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,23 @@
-import { text } from 'stream/consumers';
+import { text } from "stream/consumers";
+const colors = require("tailwindcss/colors");
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        onest: ['Onest', 'sans-serif'],
+        onest: ["Onest", "sans-serif"],
       },
     },
     colors: {
+      ...colors,
       primary: "#ED264E",
       secondary: "#F9DC5C",
       offwhite: "#F4FFFD",
       text: "#011936",
       themebg: "#465362",
-
     },
     plugins: [],
-  }
-}
-
+  },
+};
