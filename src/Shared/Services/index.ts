78484d5a1cd5a28d6d804c022a8953b54274117e
ReneNieto <rene.nieto@idoagency.com>
2024-04-22 01:46:47 -0600

    added token validation to redirect

--- src/Shared/Services/index.ts
+++ src/Shared/Services/index.ts
@@ -1,30 +1,47 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosInstance } from "axios"
 
 export enum PATH_LIST {
-  AGENCIES = "/agencies",
-  PROFILES = "/profiles",
-  DESTINATIONS = "/destinations",
+	AGENCIES = "/agencies",
+	PROFILES = "/profiles",
+	DESTINATIONS = "/destinations",
 }
 export interface IUserData {
-  jwtToken: string;
+	jwtToken: string
 }
 
 export const createAxiosInstance = (
-  // userData: IUserData,
-  PATH?: string
+	// userData: IUserData,
+	PATH?: string
 ): AxiosInstance => {
-  const baseURL = `${import.meta.env.VITE_API_BASE_URL as string}${PATH}`;
+	const baseURL = `${import.meta.env.VITE_API_BASE_URL as string}${PATH}`
 
-  const token = sessionStorage.getItem("accessToken");
-  const headers = {
-    Authorization: `Bearer ${token}`,
-    "Content-Type": "application/json",
-  };
+	const token = sessionStorage.getItem("accessToken")
+	const headers = {
+		Authorization: `Bearer ${token}`,
+		"Content-Type": "application/json",
+	}
 
-  const clientAxios = axios.create({
-    baseURL,
-    headers,
-  });
+	const clientAxios = axios.create({
+		baseURL,
+		headers,
+	})
 
-  return clientAxios;
-};
+	return clientAxios
+}
+
+export const getUserByToken = async (token: string) => {
+	try {
+		const response = await axios.get(
+			`${import.meta.env.VITE_API_BASE_URL}/getUserByToken`,
+			{
+				headers: {
+					Authorization: `Bearer ${token}`,
+					"Content-Type": "multipart/form-data",
+				},
+			}
+		)
+		return response
+	} catch (error) {
+		return error
+	}
+}
